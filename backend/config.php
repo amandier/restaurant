24923d008baefdcea4d39602a3493ebbab84f603
amandier <ahmedallal3840@gmail.com>
2025-09-17 17:55:51 +0100

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,1 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "amandier";
+$user = "root";  // à adapter selon ton hébergeur
+$pass = "";      // à adapter selon ton hébergeur
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
 
